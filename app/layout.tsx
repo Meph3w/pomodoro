6800c77bc8afdbdbd91eee34049773bfe1818f20
Meph3w <65207295+Meph3w@users.noreply.github.com>
2025-03-13 17:24:45 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMF49D9N');</script>
+<!-- End Google Tag Manager -->
         <link
           rel="icon"
           href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/ALARMEVERMELHO-XAfCNsHBhbml97ZqHiXPmonFvz8VPM.png"
@@ -27,6 +34,10 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMF49D9N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
